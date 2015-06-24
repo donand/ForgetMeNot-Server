78c1d9cacb226f6f79cc7a7d41ba838e44e30fe9
donand <donati.andrea.00@gmail.com>
2015-06-24 11:23:07 +0200

    Modificata la classe ConnectionManager

--- src/com/forgetmenot/ConnectionManager.java
+++ src/com/forgetmenot/ConnectionManager.java
@@ -7,12 +7,20 @@ public class ConnectionManager {
     }
     ;
     
-    private static Connection conn = null;
+    private static boolean driverLoaded = false;
     private static final String MY_DRIVER = "org.postgresql.Driver";
     private static final String MY_URL = "jdbc:postgresql://localhost/postgres";
     private static final String LOGIN = "postgres";
     private static final String PASSWD = "postgres";
+    
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		if (!driverLoaded) {
+			Class.forName(MY_DRIVER);
+			driverLoaded = true;
+		}
+		return DriverManager.getConnection(MY_URL, LOGIN, PASSWD);
+		
+		/*
         if (conn == null) {
             Class.forName(MY_DRIVER);
             conn = DriverManager.getConnection(MY_URL, LOGIN, PASSWD);
@@ -20,10 +28,11 @@ public class ConnectionManager {
             conn = DriverManager.getConnection(MY_URL, LOGIN, PASSWD);
         } else // In questo caso si e’ verificato un comportamento
         // non atteso. Inserisco comandi che corrispondono
-        // ad una politica da seguire in caso di errore
+        // ad una politica da seguire in caso di errore 
         {
             conn.rollback(); //si e’ scelto di disfare tutto il lavoro eseguito dall’ultimo commit.
         }
         return conn;
+        */
     }
 }
