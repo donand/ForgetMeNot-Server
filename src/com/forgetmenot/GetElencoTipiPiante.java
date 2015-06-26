38466e38086a80c165a6fae9b791c99618f7d3ad
donand <donati.andrea.00@gmail.com>
2015-06-26 10:47:58 +0200

    Aggiunta richiesta per tutti i tipi di piante con relative immagini

---
+++ src/com/forgetmenot/GetElencoTipiPiante.java
@@ -0,0 +1,80 @@
+package com.forgetmenot;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.forgetmenot.database.ConnectionManager;
+
+/**
+ * Servlet implementation class GetElencoTipiPiante
+ */
+@WebServlet("/GetElencoTipiPiante")
+public class GetElencoTipiPiante extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetElencoTipiPiante() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+			conn = ConnectionManager.getConnection();
+			stmt = conn.createStatement();
+			String query = "SELECT nome, immagine "
+					+ "FROM pianta";
+			ResultSet rs = stmt.executeQuery(query);
+			JSONArray result = new JSONArray();
+			while (rs.next()) {
+				JSONObject obj = new JSONObject();
+				obj.put("nome", rs.getString("nome"));
+				obj.put("immagine", rs.getString("immagine"));
+				result.put(obj);
+			}
+			response.getWriter().write(result.toString());
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+		catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		catch (JSONException e2) {
+			e2.printStackTrace();
+		}
+		finally {
+			if (conn != null) {
+				try {
+					stmt.close();
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
