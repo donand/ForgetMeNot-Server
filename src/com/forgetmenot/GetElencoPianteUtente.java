c8c5503c4a402e3c0cd3dc5e9502f42a750535be
donand <donati.andrea.00@gmail.com>
2015-06-27 16:58:54 +0200

    Aggiunto il campo "immagine" nel json di risposta nella richiesta per l'elenco delle piante dell'utente

--- src/com/forgetmenot/GetElencoPianteUtente.java
+++ src/com/forgetmenot/GetElencoPianteUtente.java
@@ -50,7 +50,8 @@ public class GetElencoPianteUtente extends HttpServlet {
 		String queryElencoPiante = "SELECT p2.id as id, p2.nomeassegnato as nomeAssegnato, "
 				+ "p4.nome as nomePianta, p2.dataultimaacqua as ultimaAcqua, "
 				+ "p2.dataultimofertilizzante as ultimoFertilizzante, "
-				+ "p4.acqua as intervalloAcqua, p4.intervalloconcimazione as intervalloConcimazione "
+				+ "p4.acqua as intervalloAcqua, p4.intervalloconcimazione as intervalloConcimazione, "
+				+ "p4.immagine as immagine "
 				+ "FROM possessore as p1, possesso as p2, posseduta as p3, pianta as p4 "
 				+ "WHERE p1.idutente = ? and p1.idpossesso = p2.id and "
 				+ "p2.id = p3.idpossesso and p3.idpianta = p4.id";
@@ -69,6 +70,7 @@ public class GetElencoPianteUtente extends HttpServlet {
 				obj.put("ID", rs.getInt("id"));
 				obj.put("nomeAssegnato", rs.getString("nomeAssegnato"));
 				obj.put("nomePianta", rs.getString("nomePianta"));
+				obj.put("immagine", rs.getString("immagine"));
 				obj.put("livelloAcqua", calcolaLivello(rs.getDate("ultimaAcqua"), rs.getInt("intervalloAcqua")));
 				obj.put("livelloConcimazione", calcolaLivello(rs.getDate("ultimoFertilizzante"), rs.getInt("intervalloConcimazione")));
 				result.add(obj);
