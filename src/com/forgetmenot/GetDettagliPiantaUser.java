67a62c15315e6a86bb1bda378b3001c39d8f3d9e
donand <donati.andrea.00@gmail.com>
2015-07-02 16:36:17 +0200

    Aggiunti i campi per vedere se una pianta ha le notifiche attivate o meno.

--- src/com/forgetmenot/GetDettagliPiantaUser.java
+++ src/com/forgetmenot/GetDettagliPiantaUser.java
@@ -45,42 +45,46 @@ public class GetDettagliPiantaUser extends HttpServlet {
          */
         @SuppressWarnings("unchecked")
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        		// TODO Auto-generated method stub
+        	// TODO Auto-generated method stub
                
-                //prende come parametro l'id del Possesso(entità che collega un user alla sua pianta)
-                int id=Integer.parseInt(request.getParameter("id"));
-                try{
+            //prende come parametro l'id del Possesso(entità che collega un user alla sua pianta)
+            int id=Integer.parseInt(request.getParameter("id"));
+            try{
                        
-                Connection con = ConnectionManager.getConnection();
-                String query="SELECT possesso.indirizzo as indirizzo, possesso.GPSLat as gpslat, possesso.GPSLong as gpslong, "
-                                + "pianta.immagine as immagine, "
-                                + "pianta.descrizioneConcimazione as descrizioneConcimazione, pianta.descrizioneAcqua as descrizioneAcqua, "
-                                + "pianta.luce as luce, possesso.dataUltimaAcqua as dataUltimaAcqua, possesso.dataUltimoFertilizzante as dataUltimoFertilizzante, pianta.acqua, pianta.intervalloConcimazione "
-                                + "FROM possesso, posseduta, pianta "
-                                + "WHERE posseduta.idpossesso=? AND posseduta.idpossesso=possesso.id AND posseduta.idpianta=pianta.id";
+            Connection con = ConnectionManager.getConnection();
+            String query="SELECT possesso.indirizzo as indirizzo, possesso.GPSLat as gpslat, possesso.GPSLong as gpslong, "
+                            + "pianta.immagine as immagine, "
+                            + "pianta.descrizioneConcimazione as descrizioneConcimazione, pianta.descrizioneAcqua as descrizioneAcqua, "
+                            + "pianta.luce as luce, possesso.dataUltimaAcqua as dataUltimaAcqua, possesso.dataUltimoFertilizzante as dataUltimoFertilizzante, "
+                            + "pianta.acqua, pianta.intervalloConcimazione, "
+                            + "possesso.notificheAcqua as notificheAcqua, possesso.notificheFertilizzante as notificheFertilizzante "
+                            + "FROM possesso, posseduta, pianta "
+                            + "WHERE posseduta.idpossesso=? AND posseduta.idpossesso=possesso.id AND posseduta.idpianta=pianta.id";
                
-                PreparedStatement stmt=con.prepareStatement(query);
-                stmt.setInt(1, id);
-                ResultSet rs= stmt.executeQuery();
-                //StringWriter out=new StringWriter();
-                rs.next();
+            PreparedStatement stmt=con.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs= stmt.executeQuery();
+            //StringWriter out=new StringWriter();
+            rs.next();
                
-                JSONObject obj=new JSONObject();
-                obj.put("dataUltimaAcqua", new Date(rs.getLong("dataUltimaAcqua")).toString());
-                obj.put("dataUltimoFertilizzante", new Date(rs.getLong("dataUltimoFertilizzante")).toString());
-                obj.put("acqua", rs.getInt("acqua"));
-                obj.put("intervalloConcimazione", rs.getInt("intervalloConcimazione"));
-                obj.put("luce",rs.getInt("luce"));
-                obj.put("descrizioneAcqua",rs.getString("descrizioneAcqua"));
-                obj.put("descrizioneConcimazione",rs.getString("descrizioneConcimazione"));
-                obj.put("gpslat", rs.getFloat("GPSLat"));
-                obj.put("gpslong", rs.getFloat("GPSLong"));
-                obj.put("indirizzo", rs.getString("indirizzo"));
-                obj.put("livelloAcqua", Utils.calcolaLivello(rs.getLong("dataUltimaAcqua"), rs.getInt("acqua")));
-        		obj.put("livelloConcimazione", Utils.calcolaLivello(rs.getLong("dataUltimoFertilizzante"), rs.getInt("intervalloConcimazione")));
-                       
-                //String jsonText = out.toString();
-                String jsonText = obj.toString();
+            JSONObject obj=new JSONObject();
+            obj.put("dataUltimaAcqua", new Date(rs.getLong("dataUltimaAcqua")).toString());
+            obj.put("dataUltimoFertilizzante", new Date(rs.getLong("dataUltimoFertilizzante")).toString());
+            obj.put("acqua", rs.getInt("acqua"));
+            obj.put("intervalloConcimazione", rs.getInt("intervalloConcimazione"));
+            obj.put("luce",rs.getInt("luce"));
+            obj.put("descrizioneAcqua",rs.getString("descrizioneAcqua"));
+            obj.put("descrizioneConcimazione",rs.getString("descrizioneConcimazione"));
+            obj.put("gpslat", rs.getFloat("GPSLat"));
+            obj.put("gpslong", rs.getFloat("GPSLong"));
+            obj.put("indirizzo", rs.getString("indirizzo"));
+            obj.put("livelloAcqua", Utils.calcolaLivello(rs.getLong("dataUltimaAcqua"), rs.getInt("acqua")));
+            obj.put("livelloConcimazione", Utils.calcolaLivello(rs.getLong("dataUltimoFertilizzante"), rs.getInt("intervalloConcimazione")));
+            obj.put("notificheAcqua", rs.getBoolean("notificheAcqua"));
+            obj.put("notificheFertilizzante",  rs.getBoolean("notificheFertilizzante"));
+            
+            //String jsonText = out.toString();
+            String jsonText = obj.toString();
             OutputStreamWriter outR = new OutputStreamWriter(response.getOutputStream());
             outR.write(jsonText);
             outR.flush();
@@ -92,6 +96,8 @@ public class GetDettagliPiantaUser extends HttpServlet {
         catch(ClassNotFoundException e){
         }
         catch(SQLException e){
+        	e.printStackTrace();
+        	response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
         }
  
